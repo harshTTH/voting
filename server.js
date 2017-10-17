ac07a50811062f94931c910cb1d7f2619927ccce
Harsh Rai <harshrai456@gmail.com>
2017-10-17 11:06:46 +0000

    transferring locally

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use('/assets',express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=>{
+   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(process.env.PORT);
